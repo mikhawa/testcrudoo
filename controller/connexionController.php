8b62ee3cb64ed06068d16e2e6104afcada09fcd5
mikhawa <michaeljpitz@gmail.com>
2019-09-24 16:32:15 +0200

    new theuser() avec un var_dump de  et theuser

--- controller/connexionController.php
+++ controller/connexionController.php
@@ -4,6 +4,11 @@
 if(isset($_POST['theuserlogin'])){
   
     // création d'une instance de type theuser
+    $theuserInstance = new theuser($_POST);
+    echo "<hr>";
+    var_dump($_POST,$theuserInstance);
+    echo "</hr>";
+    
     
 // sinon accueil de la connexion    
 }else{
