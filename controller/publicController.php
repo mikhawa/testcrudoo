852a0db36b3d09edb0f1724ae06eeabca3924af4
mikhawa <michaeljpitz@gmail.com>
2019-09-26 12:12:32 +0200

    Appel de la déconnexion avec la variable ['disconnect'] dans admin et public

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,3 +1,16 @@
 <?php
+/*
+ * on veut se déconnecter
+ */
+if(isset($_GET['disconnect'])){
+    
+    $theuserM->disconnectTheuser();
+    
+/*
+ * Accueil de l'utilisateur
+ */ 
+}else{
+    
+    echo $twig->render("public/accueilPublic.html.twig");
 
-echo $twig->render("public/accueilPublic.html.twig");
+}
